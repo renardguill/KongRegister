69848441bde3c3ec7e61584007967995dc75e0bc
Guillaume Renard <renardguillaume@gmail.com>
2018-05-24 15:26:11 +0200

    Remove unnecessary usings

--- src/KongRegister/Business/KongRegisterBusiness.cs
+++ src/KongRegister/Business/KongRegisterBusiness.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Sockets;
-using System.Text;
 using System.Threading.Tasks;
 using KongRegister.Business.Interfaces;
 using KongRegister.Extensions;
